0eb70118a8870e3cde52149c26987a8a2bc73da1
Chintan Patel <patelchintan843@gmail.com>
2022-12-30 14:33:52 +0530

    upd: all

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const staticPath = path.join(__dirname, "/public");
+
+app.use(express.static(staticPath));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
