a0cfa7b7da2dbcaf36b8be1fe02bbb53e46dd7e9
Chintan Patel <patelchintan843@gmail.com>
2022-12-30 14:42:24 +0530

    upd: index & app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const staticPath = path.join(__dirname, "/public");
+const staticPath = path.join(__dirname);
 
 app.use(express.static(staticPath));
 
